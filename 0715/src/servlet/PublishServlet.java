31d5a2b3c3585043d962c78066e9ebcfff15ec11
leechenice <leechenice@gmail.com>
2020-07-17 10:57:51 +0800

    :>

---
+++ 0715/src/servlet/PublishServlet.java
@@ -0,0 +1,56 @@
+package servlet;
+
+
+import model.Article;
+import model.User;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.MultipartConfig;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.*;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.sql.SQLException;
+
+@MultipartConfig
+@WebServlet("/publish")
+public class PublishServlet extends HttpServlet {
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        HttpSession session = req.getSession();
+        User user = (User) session.getAttribute("user");
+        if (user == null) {
+            resp.sendRedirect("login.html");
+            return;
+        }
+
+        req.setCharacterEncoding("utf-8");
+        String title = req.getParameter("title");
+        String content = req.getParameter("content");
+
+        Part imagePart = req.getPart("image");
+        InputStream is = imagePart.getInputStream();
+        byte[] buffer = new byte[1024];
+        try (OutputStream os = new FileOutputStream("E:\\比特科技\\课程\\JavaWeb\\2020年-火箭班2\\2020-07-13-网络-HTTP协议-博客系统\\保存上传过来的文件\\Hello.gif")) {
+            while (true) {
+                int len = is.read(buffer);
+                if (len == -1) {
+                    // 读到 EOS,所有数据全部读完了
+                    break;
+                }
+                os.write(buffer, 0, len);
+            }
+            os.flush();
+        }
+
+        try {
+            Article.publish(user.id, title, content);
+        } catch (SQLException e) {
+            throw new ServletException(e);
+        }
+
+        resp.sendRedirect("/");
+    }
+}
---
+++ 0715/src/util/DBUtil.java
@@ -0,0 +1,27 @@
+package util;
+import com.mysql.jdbc.jdbc2.optional.MysqlDataSource;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+public class DBUtil {
+    public static final DataSource dataSource = initDataSource();
+
+    private static DataSource initDataSource() {
+        MysqlDataSource mysqlDataSource = new MysqlDataSource();
+        mysqlDataSource.setServerName("127.0.0.1");
+        mysqlDataSource.setPort(3306);
+        mysqlDataSource.setUser("root");
+        mysqlDataSource.setPassword("92727");
+        mysqlDataSource.setDatabaseName("blog");
+        mysqlDataSource.setCharacterEncoding("utf8");
+        mysqlDataSource.setUseSSL(false);
+
+        return mysqlDataSource;
+    }
+
+    public static Connection getConnection() throws SQLException {
+        return dataSource.getConnection();
+    }
+}
